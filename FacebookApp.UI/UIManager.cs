0aaa56b3e71f9d1231d839c5c6efea921a52f2bf
Dudi <dudi.chen@gmail.com>
2020-04-09 06:59:18 +0300

    Removed redundant instances of ApplicationSettings and LoginManager from UIManager

--- FacebookApp.UI/UIManager.cs
+++ FacebookApp.UI/UIManager.cs
@@ -5,14 +5,10 @@ namespace FacebookApp.UI
 {
     public class UIManager
     {
-        private ApplicationSettings m_ApplicationSettings;
-        private LoginManager m_LoginManager;
         private FormMain m_MainForm;
 
         public UIManager()
         {
-            m_ApplicationSettings = ApplicationSettings.Instance;
-            m_LoginManager = LoginManager.Instance;
             m_MainForm = new FormMain();
         }
 
